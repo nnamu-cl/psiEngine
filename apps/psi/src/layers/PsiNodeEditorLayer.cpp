9c50b585987753ab0e44c9e1b3c90856ed96cc69
nnamu-cl <142959510+nnamu-cl@users.noreply.github.com>
2026-02-07 13:45:12 +0300

    Added general node driven support

--- apps/psi/src/layers/PsiNodeEditorLayer.cpp
+++ apps/psi/src/layers/PsiNodeEditorLayer.cpp
@@ -4,6 +4,7 @@
 #include "nodes/ValueNodes.h"
 #include "imgui.h"
 #include "imgui_node_editor.h"
+#include "implot.h"
 
 namespace ed = ax::NodeEditor;
 
@@ -19,6 +20,9 @@ PsiNodeEditorLayer::~PsiNodeEditorLayer()
 
 void PsiNodeEditorLayer::OnAttach()
 {
+    // Create ImPlot context
+    ImPlot::CreateContext();
+
     // Create drawers
     if (m_WorldLayer)
     {
@@ -32,6 +36,9 @@ void PsiNodeEditorLayer::OnAttach()
 void PsiNodeEditorLayer::OnDetach()
 {
     // Node system drawer will clean up its own context in its destructor
+
+    // Destroy ImPlot context
+    ImPlot::DestroyContext();
 }
 
 void PsiNodeEditorLayer::OnUpdate(float ts)
@@ -72,6 +79,30 @@ void PsiNodeEditorLayer::RenderNodeEditor()
     if (m_NodeSystemDrawer)
     {
         m_NodeSystemDrawer->DrawNodeGraph();
+
+        return;
+        static float xs1[1001], ys1[1001];
+        for (int i = 0; i < 1001; ++i) {
+            xs1[i] = i * 0.001f;
+            ys1[i] = 0.5f + 0.5f * sinf(50 * (xs1[i] + (float)ImGui::GetTime() / 10));
+        }
+        static double xs2[20], ys2[20];
+        for (int i = 0; i < 20; ++i) {
+            xs2[i] = i * 1/19.0f;
+            ys2[i] = xs2[i] * xs2[i];
+        }
+        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+        ImGui::Begin("My Window" );
+        ImPlotAxisFlags flags = ImPlotAxisFlags_AutoFit;
+        if (ImPlot::BeginPlot("Line Plots" )) {
+            ImPlot::SetupAxes("x","y", flags, flags);
+            ImPlot::PlotLine("f(x)", xs1, ys1, 1001);
+            ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
+            ImPlot::PlotLine("g(x)", xs2, ys2, 20,ImPlotLineFlags_Segments);
+            ImPlot::EndPlot();
+        }
+        ImGui::End();
+
     }
 }
 
