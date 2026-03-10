8ccdd548b91bb9f652745070d7e3cfcd890626ad
nnamu-cl <142959510+nnamu-cl@users.noreply.github.com>
2026-03-10 20:00:03 +0300

    done

--- apps/decisions/data_types/cashflow.h
+++ apps/decisions/data_types/cashflow.h
@@ -1,4 +1,15 @@
 #pragma once
+#include <ctime>
 
 
+template <typename c_type, typename p_type> struct CashFlow
+{
 
+public:
+    // Amount
+    c_type amount;
+    // Date
+    time_t time;
+    // Probability of occurring
+    p_type probability;
+} ;
--- apps/decisions/main.cpp
+++ apps/decisions/main.cpp
@@ -1,9 +1,26 @@
+#include <filesystem>
 #include <iostream>
+
+
+
+#include "data_types/cashflow.h"
 
 int main() {
 
 
     // Load the data to be used (csv form)
+    std::vector<CashFlow<float, float>> cashflows;
+    {
+
+        std::filesystem::path home_path = 
+
+        // We want the following kind of data
+        // Expected Incomes
+        // Expected Expenses
+
+
+
+    }
     // Pack data into correct datasets - probably in financial terms
     // Perform needed calculations
     // Print out/visualize important information
