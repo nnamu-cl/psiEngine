1b5e3324cec3ae9e99c2321f469572f3d45bb812
nnamu-cl <142959510+nnamu-cl@users.noreply.github.com>
2026-03-10 13:41:39 +0300

    made minor chages

---
+++ apps/decisions/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+
+    // Load the data to be used (csv form)
+    // Pack data into correct datasets - probably in financial terms
+    // Perform needed calculations
+    // Print out/visualize important information
+
+    return 0;
+}
