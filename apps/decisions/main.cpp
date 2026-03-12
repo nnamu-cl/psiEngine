0e89f5b81a0899c50b89eb6a2f72d3dc59b7dc8c
nnamu-cl <namusanganicho@gmail.com>
2026-03-12 13:22:31 +0300

    added sol2

--- apps/decisions/main.cpp
+++ apps/decisions/main.cpp
@@ -1,11 +1,35 @@
+#define SOL_ALL_SAFE_OPTS 1
+#define SOL_USING_CXX_LUA 0
+#define SOL_USING_CXX_LUAJIT 0
 #include <filesystem>
 #include <iostream>
 #include <vector>
 
-
 #include "directory_tools.h"
-#include "data_types/cashflow.h"
+
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "luajit.h"
+}
+
 #include "engine/Game.h"
+#include "sol2/sol.hpp"
+
+
+static int wrap_exceptions(lua_State *L, lua_CFunction f)
+{
+    try {
+        return f(L);  // Call wrapped function and return result.
+    } catch (const char *s) {  // Catch and convert exceptions.
+        lua_pushstring(L, s);
+    } catch (std::exception& e) {
+        lua_pushstring(L, e.what());
+    } catch (...) {
+        lua_pushliteral(L, "caught (...)");
+    }
+    return lua_error(L);  // Rethrow as a Lua error.
+}
 
 int main()
 {
@@ -63,6 +87,14 @@ int main()
 
         game.PrintHead();
 
+
+        sol::state lua;
+        if (!lua.lua_state()) {
+            std::cerr << "Failed to create lua state!" << std::endl;
+            return 1;
+        }
+        lua.open_libraries(sol::lib::base, sol::lib::package);
+        lua.script("print('bark bark bark!')");
 
 
 
